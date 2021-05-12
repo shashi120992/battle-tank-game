522f0234e599ed22bca98d52196d0476e602b436
shashikanthbhat12 <shashi120992@gmail.com>
2021-05-12 19:23:30 +0530

    Imported joystick and attached

---
+++ Assets/Scripts/TankMovementJoystick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TankMovementJoystick : MonoBehaviour
+    {
+
+        public Animator animator;                                                   //for animation
+        public float speed;
+        public Joystick joystick;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            //float horizontal = Input.GetAxisRaw("Horizontal");
+            float horizontal = joystick.Horizontal;
+            float Vertical = joystick.Vertical;
+            RunAnim(horizontal);
+            playhorizontal(horizontal, Vertical);
+           
+
+        }
+       
+        private void RunAnim(float horizontal)
+        {
+            //move charector horizontally
+            Vector3 position = transform.position;
+            position.x += horizontal * speed * Time.deltaTime;
+            transform.position = position;
+
+        }
+
+        private void playhorizontal(float horizontal, float Vertical)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(horizontal)); //changing speed to abs
+
+            Vector3 scale = transform.localScale;
+            if (horizontal < 0)
+            {
+                scale.x = -1f * Mathf.Abs(scale.x); // changig speed to abs
+            }
+            else if (horizontal > 0)
+            {
+                scale.x = Mathf.Abs(horizontal);
+            }
+            transform.localScale = scale;
+
+        }
+    }
+}
---
+++ Assets/Scripts/TankView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TankView : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
